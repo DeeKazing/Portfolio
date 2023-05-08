3fe7e332b3883fe8bcc0d0c6008e1f492fb63ceb
Feridun <feridun-aydogan@hotmail.de>
2023-05-08 17:49:24 +0200

    cursor coloring

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,14 +6,5 @@ import { Component, ElementRef } from '@angular/core';
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent {
-  constructor(private elementRef: ElementRef) { }
 
-  ngOnInit() {
-    this.elementRef.nativeElement.querySelector('#entry').addEventListener('click', () => {
-      document.querySelector('#entry')!.scrollIntoView({ behavior: 'smooth' });
-    });
-    this.elementRef.nativeElement.querySelector('#contact-us').addEventListener('click', () => {
-      document.querySelector('#end')!.scrollIntoView({ behavior: 'smooth' });
-    });
-  }
 }
